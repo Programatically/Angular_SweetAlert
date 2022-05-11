cfd98a22d42124882c5ef8d063593cdd04e52dfb
daniyal-akbar <da.programatically@gmail.com>
2022-05-12 03:24:25 +0500

    Second commit

--- src/app/alertmsg/alertmsg.component.ts
+++ src/app/alertmsg/alertmsg.component.ts
@@ -4,67 +4,88 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-alertmsg',
   templateUrl: './alertmsg.component.html',
-  styleUrls: ['./alertmsg.component.css']
+  styleUrls: ['./alertmsg.component.css'],
 })
 export class AlertmsgComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  simpleAlert() {
+    Swal.fire('Welcome to Programatically');
   }
 
+  alertWithSuccess() {
+    Swal.fire('Thank you...', 'You submitted successfully!', 'success');
+  }
+  erroalert() {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong!',
+      footer: '<a href>Why do I have this issue?</a>',
+    });
+  }
+  topend() {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+  confirmBox() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        );
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
+      }
+    });
+  }
 
-  
-  simpleAlert(){  
-    Swal.fire('Hello Angular');  
-  }  
- 
-  alertWithSuccess(){  
-    Swal.fire('Thank you...', 'You submitted successfully!', 'success')  
-  }  
-  erroalert()  
-  {  
-    Swal.fire({  
-      icon: 'error',  
-      title: 'Oops...',  
-      text: 'Something went wrong!',  
-      footer: '<a href>Why do I have this issue?</a>' 
-    })  
-  }  
-  topend()  
-  {  
-    Swal.fire({  
-      position: 'top-end',  
-      icon: 'success',  
-      title: 'Your work has been saved',  
-      showConfirmButton: false,  
-      timer: 1500  
-    })  
-  }  
-  confirmBox(){  
-    Swal.fire({  
-      title: 'Are you sure want to remove?',  
-      text: 'You will not be able to recover this file!',  
-      icon: 'warning',  
-      showCancelButton: true,  
-      confirmButtonText: 'Yes, delete it!',  
-      cancelButtonText: 'No, keep it' 
-    }).then((result) => {  
- if (result.value) {  
-        Swal.fire(  
- 'Deleted!',  
- 'Your imaginary file has been deleted.',  
- 'success' 
-        )  
-      } else if (result.dismiss === Swal.DismissReason.cancel) {  
-        Swal.fire(  
- 'Cancelled',  
- 'Your imaginary file is safe :)',  
- 'error' 
-        )  
-      }  
-    })  
-  }  
-
-
+  ajax_request() {
+    Swal.fire({
+      title: 'Submit your Github username',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off',
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Look up',
+      showLoaderOnConfirm: true,
+      preConfirm: (login) => {
+        return fetch(`//api.github.com/users/${login}`)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            return response.json();
+          })
+          .catch((error) => {
+            Swal.showValidationMessage(`Request failed: ${error}`);
+          });
+      },
+      allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: `${result.value.login}'s avatar`,
+          imageUrl: result.value.avatar_url,
+        });
+      }
+    });
+  }
 }
